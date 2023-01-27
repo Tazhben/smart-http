4d8e79606373fb903bdf8d5bbe6360bac85a2569
Arsu Tazhben <tazhben.a@rahmetapp.kz>
2023-01-27 12:18:41 +0600

    http client add retry

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.2 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.37.0 // indirect
 	go.opentelemetry.io/otel v1.11.2 // indirect
 	go.opentelemetry.io/otel/metric v0.34.0 // indirect
--- http/client.go
+++ http/client.go
@@ -1,24 +1,14 @@
 package http
 
 import (
-	"context"
 	"crypto/x509"
-	"net"
 	"net/http"
 	"time"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
-func defaultTransportDialContext(dialer *net.Dialer) func(context.Context, string, string) (net.Conn, error) {
-	return dialer.DialContext
-}
-
 var defaultTransport = &http.Transport{
-	DialContext: defaultTransportDialContext(&net.Dialer{
-		Timeout:   30 * time.Second,
-		KeepAlive: 30 * time.Second,
-	}),
 	ForceAttemptHTTP2:     true,
 	MaxIdleConns:          100,
 	IdleConnTimeout:       90 * time.Second,
@@ -28,6 +18,17 @@ var defaultTransport = &http.Transport{
 
 var defaultTimeout = time.Millisecond * 300
 
+var defaultRetry = &RetryRoundTripper{
+	Retries:    2,
+	RetryDelay: time.Millisecond * 100,
+	StatusCodes: map[int]bool{
+		http.StatusInternalServerError: true,
+		http.StatusBadGateway:          true,
+		http.StatusServiceUnavailable:  true,
+		http.StatusGatewayTimeout:      true,
+	},
+}
+
 func defaultClient() *Client {
 	client := Client{}
 
@@ -36,6 +37,10 @@ func defaultClient() *Client {
 
 	// по дефолту во всех сервисах используем openTelemetry
 	client.http.Transport = otelhttp.NewTransport(client.http.Transport)
+
+	// по дефолту 2 попытки
+	client.http = NewRetryClient(client.http, defaultRetry)
+
 	return &client
 }
 
@@ -44,15 +49,41 @@ type TLSConfig struct {
 	CaCert   *x509.CertPool
 }
 
+type RetryRoundTripper struct {
+	Transport http.RoundTripper
+
+	Retries     int
+	RetryDelay  time.Duration
+	StatusCodes map[int]bool
+}
+
+func NewRetryRoundTripper(retries int, retryDelay time.Duration, statusCodes map[int]bool) *RetryRoundTripper {
+	return &RetryRoundTripper{
+		Retries:     retries,
+		RetryDelay:  retryDelay,
+		StatusCodes: statusCodes,
+	}
+}
+
 type Client struct {
 	tlsConfig TLSConfig
 	timeout   time.Duration
 
-	http http.Client
+	http *http.Client
+}
+
+func NewRetryClient(inner *http.Client, retry *RetryRoundTripper) *http.Client {
+
+	retry.Transport = inner.Transport
+
+	return &http.Client{
+		Transport: retry,
+		Timeout:   inner.Timeout,
+	}
 }
 
 // NewClient создание клиента
-func NewClient(options ...func(*Client)) http.Client {
+func NewClient(options ...func(*Client)) *http.Client {
 	client := defaultClient()
 
 	for _, o := range options {
@@ -82,4 +113,43 @@ func WithTimeout(timeout time.Duration) func(client *Client) {
 	return func(client *Client) {
 		client.http.Timeout = timeout
 	}
+}
+
+// WithRetry настройка попытки повторного запроса
+func WithRetry(retries int, retryDelay time.Duration, statusCodes []int) func(client *Client) {
+	return func(client *Client) {
+
+		statusCodesInMap := make(map[int]bool)
+		for _, status := range statusCodes {
+			if _, ok := statusCodesInMap[status]; ok {
+				continue
+			}
+			statusCodesInMap[status] = true
+		}
+
+		retry := NewRetryRoundTripper(retries, retryDelay, statusCodesInMap)
+		client.http = NewRetryClient(client.http, retry)
+	}
+}
+
+// RoundTrip написал обвертку на request Response для retry
+func (r *RetryRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	var resp *http.Response
+	var err error
+
+	for i := 0; i < r.Retries; i++ {
+
+		resp, err = r.Transport.RoundTrip(req)
+		if err != nil {
+			continue
+		}
+
+		// проверка на статусы
+		if _, ok := r.StatusCodes[resp.StatusCode]; !ok {
+			return resp, err
+		}
+
+		time.Sleep(r.RetryDelay)
+	}
+	return resp, err
 }
